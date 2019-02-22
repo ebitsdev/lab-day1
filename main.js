c36b97d5bbc7103f6746432af3ecdab5ca299503
E. Bamba <294859+ebitsdev@users.noreply.github.com>
2019-02-22 09:40:52 -0500

    Updated main.js to include comments
    Updated main.js to include comments about string literal and string concatenation

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 let yourName = prompt('What is your name?')
 let favoriteMovie = prompt('What is your favorite movie?')
 let favoriteFood = prompt('What is your favorite food')
+// We use ES6 new string literal to build our confirmation message back to the user
 confirm(`${yourName} your favorite movie is ${favoriteMovie} and you love ${favoriteFood}`)
+// In the below line, we string concatenation to build the confirmation message. This is another way to write the same thing as above.
 // let allAnswers = yourName + ' ' + 'your favorite movie is ' + ' ' + favoriteMovie + ' ' + 'and you love' + ' ' + favoriteFood
 console.log(`${yourName} your favorite movie is ${favoriteMovie} and you love ${favoriteFood}`)
 // confirm(allAnswers)
